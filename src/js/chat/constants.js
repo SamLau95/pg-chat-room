d0de86ead44e885a95381cd21207a0c3ab44fb07
Sam Lau <samlau95@gmail.com>
2015-11-16 17:08:36 -0800

    Use constants file

--- src/js/chat/constants.js
+++ src/js/chat/constants.js
@@ -1,12 +1,8 @@
 import Firebase from 'firebase';
 
-const BASE_URL = `https://research-chat-room.firebaseio.com/${getAttributeFromUrlParams(ROOM_REGEX)}`;
+const ROOT_URL = 'https://research-chat-room.firebaseio.com';
+// const ROOT_URL = `ws://127.0.1:5000`;
 
-const CHAT_CONSTANTS = {
-  ROOM_REGEX: ROOM_REGEX,
-  USER_ID_REGEX: USER_ID_REGEX,
-  BASE_URL: BASE_URL,
-  USERS_FIREBASE: new Firebase(`${BASE_URL}/users`),
+export default {
+  ROOT_URL,
 };
-
-export default CHAT_CONSTANTS;
